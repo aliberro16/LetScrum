a24ef996b6c590db1af5cc14fd45db2c02eb44b1
mazenhoballah <mazen.hoballah.mh@gmail.com>
2021-06-06 04:20:48 +0300

    todo feature backend

--- src/components/todo/Todocomp.js
+++ src/components/todo/Todocomp.js
@@ -2,6 +2,7 @@ import React, { Component, Fragment } from 'react';
 import AddTodoForm from './AddTodoForm';
 import List from './List';
 import { Paper, Grid, Typography } from '@material-ui/core';
+import { firestore } from '../../firebase/firebase.utils';
 
 const styles = {
     Paper: {
@@ -15,19 +16,80 @@ const styles = {
 class Todocomp extends Component {
     state = {
         list: {},
+        id: '',
     };
+    componentDidMount() {
+        const id = window.location.pathname.split('/')[2];
+        console.log('id => ', id);
+        this.setState({ id: id });
+        // this.addToList(this.getTodosFromFirestore(id));
+        firestore
+            .collection('users')
+            .doc(id)
+            .collection('todos')
+            .get()
+            .then((querySnapshot) => {
+                querySnapshot.forEach((doc) => {
+                    // doc.data() is never undefined for query doc snapshots
+                    console.log(doc.id, ' => ', doc.data().todo);
+                    // this.setState({list : doc.data().todo});
+                    let list = { ...this.state.list };
+                    list[`todo${Date.now()}`] = {
+                        todo: doc.data().todo,
+                        status: 'active',
+                    };
+                    this.setState({ list: list });
+                });
+            });
+    }
+
+    addTodoToFirestore = async (todo) => {
+        await firestore
+            .collection('users')
+            .doc(this.state.id)
+            .collection('todos')
+            .add({ todo });
+    };
+
+    getTodosFromFirestore = async (id) => {
+        await firestore
+            .collection('users')
+            .doc(id)
+            .collection('todo')
+            .get()
+            .then((doc) => {
+                if (doc.exists) {
+                    //save user info
+                    return doc.todo;
+                } else {
+                    //redirect to HomePage
+                }
+            });
+    };
+
     addToList = (todo) => {
         let list = { ...this.state.list };
         list[`todo${Date.now()}`] = {
             todo: todo,
             status: 'active',
         };
-
+        this.addTodoToFirestore(todo);
         this.setState({ list });
     };
+
     deleteTodo = (key) => {
         let list = { ...this.state.list };
+        firestore
+            .collection('users')
+            .doc(this.state.id)
+            .collection('todos')
+            .where('todo', '==', list[key]['todo'].toString())
+            .get()
+            .then((querySnapShot) => {
+                querySnapShot.forEach((doc) => doc.ref.delete());
+            });
         list[key] = null;
+
 
         this.setState({ list });
     };
@@ -43,7 +105,6 @@ class Todocomp extends Component {
             todo: todo,
             status: 'active',
         };
-
         this.setState({ list });
     };
     render() {
