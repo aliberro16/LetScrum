e18d40fd8c13d4962428745e4b97cad2c1b7f37d
mazenhoballah <mazen.hoballah.mh@gmail.com>
2021-06-08 05:04:01 +0300

    fetching data from firestore buttons not functioning

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import SignUpPage from './pages/signup-page/signup-page.component';
 import LandingScreen from './pages/landing-screen/landingscreen.component';
 import EducationPage from './pages/education-page/educationpage.component';
 import ScrollToTop from '../src/StartFromTop';
-import waitingroom from './pages/waiting-room/waitingroom';
+import WaitingRoom from './pages/waiting-room/waitingroom.component';
 import CreateProject from './pages/create-project-page/create-project.component';
 import HomePage from './pages/home-page/homepage';
 import Profile from './pages/profile-page/profile';
@@ -17,6 +17,7 @@ import ProductBacklog from './pages/product-backlog/productbacklog';
 import PendingProject from './pages/pending-project/PendingProject';
 import JoinProjectPage from './pages/join-project-page/join-project.component';
 import ChooseProjectPage from './pages/choose-project-page/choose-project-page.component';
+import BacklogTabPanel from './pages/tab.component'
 import './App.css';
 
 class App extends React.Component {
@@ -51,13 +52,14 @@ class App extends React.Component {
                         <Route exact path='/project' component={ProjectPage} />
                         <Route exact path='/learn' component={EducationPage} />
                         <Route exact path='/work/:id/project/create-project' component={CreateProject} />
-                        <Route exact path='/work/:id/project/waiting-room' component={waitingroom}/> 
+                        <Route exact path='/work/:id/project/waiting-room' component={WaitingRoom}/> 
                         <Route exact path='/work/:id/home' component={HomePage}/> 
                         <Route exact path='/work/:id/profile' component={Profile}/>
                         <Route exact path='/work/:id/productbacklog' component={ProductBacklog}/>  
                         <Route exact path='/work/:id/project/pending-request' component={PendingProject}/>  
                         <Route exact path='/work/:id/project/join-project' component={JoinProjectPage} />
                         <Route exact path='/work/:id/project/choose-project' component={ChooseProjectPage} />
+                        <Route exact path='/work/:id/project/tab' component={BacklogTabPanel} />
                         <Route
                             exact
                             path='/signin'
