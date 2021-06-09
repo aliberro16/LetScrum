ca93eb798fa586bf395c2aab868825b040ff10fa
aliberro16 <ali.berro16@hotmail.com>
2021-06-09 19:40:14 +0300

    adding project info card to home ui

--- src/pages/home-page/homepage.js
+++ src/pages/home-page/homepage.js
@@ -3,16 +3,26 @@ import ReactDOM from "react-dom";
 import Todocomp from "../../components/todo/Todocomp";
 import SideBar from "../../components/side-bar/side-bar.component";
 import "./homepage.css";
-
+import CardInfo from "../../components/card/HomeCard";
+import styled from "styled-components";
 const HomePage = () => {
   return (
     <div>
       <SideBar />
-      <div className ='container'>
-      <Todocomp/>
-      </div>
+      <Container>
+        <div>
+          <CardInfo />
+        </div>
+        <div className="container">
+          <Todocomp />
+        </div>
+      </Container>
     </div>
   );
 };
 
 export default HomePage;
+const Container = styled.div`
+  margin-left: 75px;
+  display: flex;
+`;
